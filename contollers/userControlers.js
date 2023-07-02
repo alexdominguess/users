5097fbe28550ba752a3c2c1014c2eb2acf60662d
Alex Domingues Da Silva <alexdomi@br.ibm.com>
2023-07-02 11:49:27 -0300

    small changes to improve the code

--- contollers/userControlers.js
+++ contollers/userControlers.js
@@ -1,23 +1,25 @@
 import { createNewUser, checkEmail } from "../models/userCreation.js";
 
-export const newUserHandler = async (req, res) =>{
-    const email  = req.body.email
-    const pwd  = req.body.password
-    try{
-        if(email && pwd){
-            let result = await checkEmail(email)
-            if(!result.valid){
-                res.status(409).json({message:result.message})
-            }else{
-                result = await createNewUser(email, pwd)
-                if(result.status == 200){
-                    res.status(200).json({message:"User created successfully"})
-                }
-            }
-        }else{
-            res.status(400).json({message:"Missing email and/or password"})
-        }
-    }catch(error){
-        res.status(500).json({error:error.message})
+export const newUserHandler = async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    if (!email || !password) {
+      return res.status(400).json({ message: "Missing email and/or password" });
     }
-}
+  
+    try {
+      const result = await checkEmail(email);
+      if (!result.valid) {
+        return res.status(409).json({ message: result.message });
+      }
+  
+      const createUserResult = await createNewUser(email, password);
+      if (createUserResult.status === 200) {
+        return res.status(200).json({ message: "User created successfully" });
+      }
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  };
+  
